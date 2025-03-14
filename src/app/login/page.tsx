949011ca4789adc4d00a7ba2a8a0c26255285572
Yosip Mike Colin <vendeyaonline10@gmail.com>
2025-03-14 11:35:22 -0500

    Settings

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,23 +4,14 @@ import { useMutationLoginUser } from "@/api/mutation";
 import { Button, Card } from "@/components/ui";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useState } from "react";
 import toast from "react-hot-toast";
 
 const Login = () => {
-  const [activeLogin, setActiveLogin] = useState(true);
   const [values, setValues] = useState({ email: "", password: "" });
   const [isError, setIsError] = useState(false);
   const { mutateAsync, isLoading } = useMutationLoginUser();
   const router = useRouter();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token_vendeyaonline");
-    if (token) {
-      setActiveLogin(false);
-      router.push("/");
-    }
-  }, [router]);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -51,52 +42,48 @@ const Login = () => {
   };
 
   return (
-    !activeLogin && (
-      <section className="p-3 absolute left-0 right-0 top-0 h-full bg-white flex justify-center items-center">
-        <Card className="w-[500px] sm:max-w-full mt-5 p-5 relative">
-          <form onSubmit={handleSubmit}>
-            <h1 className="text-xl font-bold"> Iniciar sesión</h1>
-            <div className="mt-10">
-              <label className="text-slate-500">Correo electrónico</label>
-              <Input
-                type="text"
-                name="email"
-                value={values.email}
-                placeholder="yosip@gmail.com"
-                onChange={(e) =>
-                  setValues({ ...values, email: e.target.value })
-                }
-              />
-            </div>
+    <section className="p-3 absolute left-0 right-0 top-0 h-full bg-white flex justify-center items-center">
+      <Card className="w-[500px] sm:max-w-full mt-5 p-5 relative">
+        <form onSubmit={handleSubmit}>
+          <h1 className="text-xl font-bold"> Iniciar sesión</h1>
+          <div className="mt-10">
+            <label className="text-slate-500">Correo electrónico</label>
+            <Input
+              type="text"
+              name="email"
+              value={values.email}
+              placeholder="yosip@gmail.com"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+            />
+          </div>
 
-            <div className="mt-5">
-              <label className="text-slate-500">Contraseña</label>
-              <Input
-                type="password"
-                name="password"
-                value={values.password}
-                placeholder="***"
-                onChange={(e) =>
-                  setValues({ ...values, password: e.target.value })
-                }
-              />
-            </div>
-            {isError && (
-              <span className="text-xs text-red-700 mt-3 block">
-                Llena todos los campos
-              </span>
-            )}
-            <Button
-              type="submit"
-              size="lg"
-              className="w-full bottom-5 text-lg mt-10 "
-            >
-              {isLoading ? <div className="spiner" /> : "Iniciar sesión"}
-            </Button>
-          </form>
-        </Card>
-      </section>
-    )
+          <div className="mt-5">
+            <label className="text-slate-500">Contraseña</label>
+            <Input
+              type="password"
+              name="password"
+              value={values.password}
+              placeholder="***"
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
+            />
+          </div>
+          {isError && (
+            <span className="text-xs text-red-700 mt-3 block">
+              Llena todos los campos
+            </span>
+          )}
+          <Button
+            type="submit"
+            size="lg"
+            className="w-full bottom-5 text-lg mt-10 "
+          >
+            {isLoading ? <div className="spiner" /> : "Iniciar sesión"}
+          </Button>
+        </form>
+      </Card>
+    </section>
   );
 };
 
